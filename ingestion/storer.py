2f0e78438db69ffee4e3850361622fee1397af1a
AkshatAsthana1412 <akshatsagittarius96@gmail.com>
2025-06-26 14:40:12 +0530

    Refactor, bug fixes

--- ingestion/storer.py
+++ ingestion/storer.py
@@ -5,7 +5,7 @@ import pandas as pd
 
 class DataFrameAdapter(ABC):
     @abstractmethod
-    def to_sql(self, destination: str, conn):
+    def to_sql(self, table_name: str, conn):
         pass
 
     @abstractmethod
@@ -16,8 +16,8 @@ class PandasDataFrameAdapter(DataFrameAdapter):
     def __init__(self, df: pd.DataFrame):
         self.df = df
 
-    def to_sql(self, destination: str, conn):
-        self.df.to_sql(destination, conn, if_exists="replace", index=False)
+    def to_sql(self, table_name: str, conn):
+        self.df.to_sql(table_name, conn, if_exists="replace", index=False)
 
     def to_csv(self, destination: str):
         self.df.to_csv(destination, index=False)
@@ -38,9 +38,8 @@ class CSVStorage(DataStorage[DataFrameAdapter]):
         data.to_csv(self.destination, index=False)
 
 class SQLiteStorage(DataStorage[DataFrameAdapter]):
-    def __init__(self, db_path: str, table_name: str):
+    def __init__(self, db_path: str):
         self.conn = sqlite3.connect(db_path)
-        self.table_name = table_name
 
-    def save(self, data: DataFrameAdapter):
-        data.to_sql(self.table_name, conn=self.conn)
+    def save(self, data: DataFrameAdapter, table_name: str):
+        data.to_sql(table_name, self.conn)
