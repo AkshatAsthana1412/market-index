2db2b9d59d41cf0d7d8b3d9213fd17614f944a8b
AkshatAsthana1412 <akshatsagittarius96@gmail.com>
2025-06-26 10:29:12 +0530

    Make DataStorage accept generic type

--- data/storer.py
+++ data/storer.py
@@ -1,17 +1,21 @@
 from abc import ABC, abstractmethod
+from typing import TypeVar, Generic
 import pandas as pd
 import sqlite3
 
-class DataStorage(ABC):
+
+T = TypeVar('T')
+
+class DataStorage(ABC, Generic[T]):
     @abstractmethod
-    def save(self, data: pd.DataFrame, destination: str = ""):
+    def save(self, data: T, destination: str = ""):
         pass
 
-class CSVStorage(DataStorage):
+class CSVStorage(DataStorage[pd.DataFrame]):
     def save(self, data: pd.DataFrame, destination: str = "output.csv"):
         data.to_csv(destination, index=False)
 
-class SQLiteStorage(DataStorage):
+class SQLiteStorage(DataStorage[pd.DataFrame]):
     def __init__(self, db_path="data.db"):
         self.conn = sqlite3.connect(db_path)
 
