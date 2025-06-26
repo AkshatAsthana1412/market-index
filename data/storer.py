d3f9d8137ac17d7d1aa7e17799e92da1de2393e2
AkshatAsthana1412 <akshatsagittarius96@gmail.com>
2025-06-26 10:59:33 +0530

    Create DataFrameAdapter abstract class, to support different types of Dataframes

--- data/storer.py
+++ data/storer.py
@@ -1,7 +1,26 @@
 from abc import ABC, abstractmethod
 from typing import TypeVar, Generic
-import pandas as pd
 import sqlite3
+import pandas as pd
+
+class DataFrameAdapter(ABC):
+    @abstractmethod
+    def to_sql(self, destination: str = ""):
+        pass
+
+    @abstractmethod
+    def to_csv(self, destination: str = ""):
+        pass
+
+class PandasDataFrameAdapter(DataFrameAdapter):
+    def __init__(self, df: pd.DataFrame):
+        self.df = df
+
+    def to_sql(self, destination: str, conn):
+        self.df.to_sql(destination, conn, if_exists="replace", index=False)
+
+    def to_csv(self, destination: str = ""):
+        self.df.to_csv(destination, index=False)
 
 
 T = TypeVar('T')
@@ -11,13 +30,13 @@ class DataStorage(ABC, Generic[T]):
     def save(self, data: T, destination: str = ""):
         pass
 
-class CSVStorage(DataStorage[pd.DataFrame]):
-    def save(self, data: pd.DataFrame, destination: str = "output.csv"):
+class CSVStorage(DataStorage[DataFrameAdapter]):
+    def save(self, data: DataFrameAdapter, destination: str = "output.csv"):
         data.to_csv(destination, index=False)
 
-class SQLiteStorage(DataStorage[pd.DataFrame]):
+class SQLiteStorage(DataStorage[DataFrameAdapter]):
     def __init__(self, db_path="data.db"):
         self.conn = sqlite3.connect(db_path)
 
-    def save(self, data: pd.DataFrame, destination: str = "prices"):
-        data.to_sql(destination, self.conn, if_exists="replace", index=False)
+    def save(self, data: DataFrameAdapter, destination: str = "prices"):
+        data.to_sql(destination, conn=self.conn)
