269096140e967010ef0e34e7d63e3e885450aeaa
AkshatAsthana1412 <akshatsagittarius96@gmail.com>
2025-06-26 11:03:16 +0530

    Match paramters in interface and concrete classes, delete default values

--- data/storer.py
+++ data/storer.py
@@ -5,11 +5,11 @@ import pandas as pd
 
 class DataFrameAdapter(ABC):
     @abstractmethod
-    def to_sql(self, destination: str = ""):
+    def to_sql(self, destination: str, conn):
         pass
 
     @abstractmethod
-    def to_csv(self, destination: str = ""):
+    def to_csv(self, destination: str):
         pass
 
 class PandasDataFrameAdapter(DataFrameAdapter):
@@ -19,7 +19,7 @@ class PandasDataFrameAdapter(DataFrameAdapter):
     def to_sql(self, destination: str, conn):
         self.df.to_sql(destination, conn, if_exists="replace", index=False)
 
-    def to_csv(self, destination: str = ""):
+    def to_csv(self, destination: str):
         self.df.to_csv(destination, index=False)
 
 
@@ -27,16 +27,16 @@ T = TypeVar('T')
 
 class DataStorage(ABC, Generic[T]):
     @abstractmethod
-    def save(self, data: T, destination: str = ""):
+    def save(self, data: T, destination: str):
         pass
 
 class CSVStorage(DataStorage[DataFrameAdapter]):
-    def save(self, data: DataFrameAdapter, destination: str = "output.csv"):
+    def save(self, data: DataFrameAdapter, destination: str):
         data.to_csv(destination, index=False)
 
 class SQLiteStorage(DataStorage[DataFrameAdapter]):
     def __init__(self, db_path="data.db"):
         self.conn = sqlite3.connect(db_path)
 
-    def save(self, data: DataFrameAdapter, destination: str = "prices"):
+    def save(self, data: DataFrameAdapter, destination: str):
         data.to_sql(destination, conn=self.conn)
